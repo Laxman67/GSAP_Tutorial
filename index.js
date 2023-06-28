52ff80f86392728429ce90fa16fe166382ee8cfb
laxman67 <laxmanhenricks94@gmail.com>
2023-06-28 22:28:29 +0530

    Gsap fromTo Understand

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 //   rotation: 360,
 //   height: 150,
 // });
+
 gsap.from("h1", {
   color: "red",
   x: 300,
@@ -18,3 +19,20 @@ gsap.from("h1", {
   duration: 1.5,
   height: 150,
 });
+
+gsap.fromTo(
+  "h2",
+  {
+    x: 200,
+    y: 150,
+    opacity: 0.3,
+    duration:2
+  },
+  {
+    x: 300,
+    y: 50,
+    scale: 2,
+    opacity: 1,
+    duration:2
+  }
+);
